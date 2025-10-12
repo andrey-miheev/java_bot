50bced10d6df636fabc994faf70823223a09c0bd
andrey-miheev <andreymikheyev2006@gmail.com>
2025-10-12 18:31:07 +0500

    Update Javabot1Application.java
    Add JavaDoc

--- src/main/java/com/task1/javabot1/Javabot1Application.java
+++ src/main/java/com/task1/javabot1/Javabot1Application.java
@@ -3,9 +3,19 @@ package com.task1.javabot1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс для запуска Telegram бота.
+ * Инициализирует Spring Boot приложение и запускает бота.
+ */
 @SpringBootApplication
 public class Javabot1Application {
 
+	/**
+	 * Точка входа в приложение.
+	 * Запускает Spring Boot контекст и активирует Telegram бота.
+	 *
+	 * @param args аргументы командной строки
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Javabot1Application.class, args);
 	}
