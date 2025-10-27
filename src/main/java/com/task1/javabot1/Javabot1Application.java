e6a8cf81572a6585b70fd628f1f1997d314c438d
andrey-miheev <andreymikheyev2006@gmail.com>
2025-10-27 19:47:59 +0500

    Update Javabot1Application.java
    Убран spring boot

--- src/main/java/com/task1/javabot1/Javabot1Application.java
+++ src/main/java/com/task1/javabot1/Javabot1Application.java
@@ -1,23 +1,22 @@
 package com.task1.javabot1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 /**
  * Главный класс для запуска Telegram бота.
- * Инициализирует Spring Boot приложение и запускает бота.
+ * Инициализирует и запускает бота.
  */
-@SpringBootApplication
 public class Javabot1Application {
-
-	/**
-	 * Точка входа в приложение.
-	 * Запускает Spring Boot контекст и активирует Telegram бота.
-	 *
-	 * @param args аргументы командной строки
-	 */
-	public static void main(String[] args) {
-
-        SpringApplication.run(Javabot1Application.class, args);
-	}
+    /**
+     * Точка входа в приложение.
+     * Запускает Telegram бота.
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) throws TelegramApiException {
+        MyTelegramBot bot = new MyTelegramBot();
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+        System.out.println("Bot started working");
+    }
 }
